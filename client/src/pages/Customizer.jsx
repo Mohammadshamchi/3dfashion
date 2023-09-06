3da2e94db1694e43b865ab0611b3415e145d0278
Mohammad Shamchi <shamchimohammad@gmail.com>
2023-09-06 17:28:56 +0300

    Added New Components For Customizer Pages

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -6,6 +6,7 @@ import config from "../config/config";
 import state from "../store";
 import { download } from "../assets";
 import { downloadCanvasToImage, reader } from "../config/helpers";
+import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
 
 const Customizer = () => {
   return <div>Customizer</div>;
