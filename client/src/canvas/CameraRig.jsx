ecbaba2ef7a4bb0d35075ce162da74c9dce27aa9
Mohammad Shamchi <shamchimohammad@gmail.com>
2023-09-07 17:08:11 +0300

    Last Push 7Sep Until T-shirt Generated 1 hour of course

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { easing } from "maath";
+import { useSnapshot } from "valtio";
 
-const CameraRig = () => {
-  return <div>CameraRig</div>;
+const CameraRig = ({ children }) => {
+  const group = useRef();
+  const snap = useSnapshot(state);
+
+  // to get the Delta useFrame This allow to executed code on every rendered frame
+  useFrame((state, delta) => {
+    const isBreakpoint = window.innerWidth <= 1260;
+    const isMobile = window.innerWidth <= 600;
+    // set the model rotation smoothly
+    easing.dampE(
+      group.current.rotation,
+      [state.pointer.y / 10, -state.pointer.x / 5, 0],
+      0.25
+    );
+  });
+
+  return <group ref={group}>{children}</group>;
 };
 
 export default CameraRig;
